2862fe14ecbeb6684b99188b99033f33f60e9b80
Hein Htet Zaw <bogeyhz.325@gmail.com>
2023-07-09 21:54:33 +0630

    Navbar contact footer done

---
+++ src/components/Navbar/MobileMenuSiderbar.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { MdClose } from "react-icons/md";
+import { FiFacebook, FiLinkedin, FiInstagram } from "react-icons/fi";
+import "./navbar.css";
+
+const MobileMenuSidebar = ({ toggleMenu, isMenuOpen, handleNavClick }) => {
+  return (
+    <div className="mobile-menu-wrapper ">
+      {/* Mobile menu overlay */}
+      <div
+        className={`mobile-menu-overlay ${isMenuOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      ></div>
+      {/* Mobile menu sidebar */}
+      <div className={`mobile-menu-sidebar ${isMenuOpen ? "open" : ""}`}>
+        {/* Mobile menu header */}
+        <div className="mobile-menu-header  my-3">
+          <div className=" flex items-center gap-4 ">
+            <img
+              src="https://cdn.pixabay.com/photo/2021/06/04/10/28/portrait-6309448_1280.jpg"
+              className="w-[50px] h-[50px] rounded-full sidebar-img border "
+              alt="img"
+            />
+            <h1 className=" text-[#c4cfde] text-xl tracking-wider uppercase font-semibold">
+              David
+            </h1>
+          </div>
+          {/* Close button */}
+          <button className="mobile-menu-close" onClick={toggleMenu}>
+            <MdClose className="mx-auto text-2xl" />
+          </button>
+        </div>
+        {/* Sidebar description */}
+        <p className="text-[#6d6d6d] mt-8  tracking-wider ">
+          Passionate web developer with a strong focus on creating immersive
+          user experiences through innovative design and cutting-edge
+          technologies.
+        </p>
+        <hr className=" my-4 opacity-30" />
+        <ul className="mobile-menu-links uppercase mb-32   text-[#1C231F]">
+          <li>
+            <a href="#home" onClick={(e) => handleNavClick(e, "home")}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#home" onClick={(e) => handleNavClick(e, "home")}>
+              Experience
+            </a>
+          </li>
+          <li>
+            <a
+              href="#education"
+              onClick={(e) => handleNavClick(e, "education")}
+            >
+              Education
+            </a>
+          </li>
+
+          <li>
+            <a href="#resume" onClick={(e) => handleNavClick(e, "resume")}>
+              Resume
+            </a>
+          </li>
+          <li>
+            <a
+              href="#portfolio"
+              onClick={(e) => handleNavClick(e, "portfolio")}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a href="#blog" onClick={(e) => handleNavClick(e, "blog")}>
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="#contact" onClick={(e) => handleNavClick(e, "contact")}>
+              Contact
+            </a>
+          </li>
+        </ul>
+        <hr className=" my-4 opacity-30" />
+        <div className="">
+          <h3 className=" uppercase tracking-wide text-lg text-[#c4cfde] font-semibold">
+            Find with me
+          </h3>
+          <div className=" mt-4 flex text-[#c4cfde] text-lg gap-3 items-center">
+            <a href="#" className=" sidebar-icon">
+              <FiFacebook />
+            </a>
+            <a href="#" className=" sidebar-icon">
+              <FiInstagram />
+            </a>
+            <a href="#" className=" sidebar-icon">
+              <FiLinkedin />
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <button className="mobile-menu-toggle" onClick={toggleMenu}>
+        <FaBars />
+      </button> */}
+    </div>
+  );
+};
+
+export default MobileMenuSidebar;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from "react";
+
+import "./navbar.css";
+
+import MobileMenuSidebar from "./MobileMenuSiderbar";
+import { FaBars } from "react-icons/fa";
+const Navbar = () => {
+  // State variables
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
+  
+   // Toggle mobile menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+   // Handle scroll event
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const isScrolled = scrollPosition > 200;
+      setIsScrolled(isScrolled);
+
+      // Check which section is currently in view
+      const sections = document.querySelectorAll("section");
+      const scrollOffset = window.innerHeight * 0.2;
+      // Adjust the scroll offset as needed
+      for (let i = sections.length - 1; i >= 0; i--) {
+        const section = sections[i];
+        const sectionTop = section.offsetTop;
+        if (scrollPosition >= sectionTop - scrollOffset) {
+          setActiveSection(section.id);
+          console.log(activeSection);
+          break;
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [activeSection]);
+
+  // Disable scroll when mobile menu is open
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.classList.add("disable-scroll");
+    } else {
+      document.body.classList.remove("disable-scroll");
+    }
+  }, [isMenuOpen]);
+  
+  // Handle navigation click
+  const handleNavClick = (event, sectionId) => {
+    event.preventDefault();
+    const section = document.getElementById(sectionId);
+    if (section) {
+      const offset = section.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({ top: offset, behavior: "smooth" });
+    }
+    setIsMenuOpen(false); // Close the mobile menu after clicking a link
+  };
+
+  return (
+    <>
+      <div
+        className={`bg-[#212428]   navbar   header  ${
+          isScrolled ? "header-sticky  stickyAdd" : "header-fixed"
+        }`}
+      >
+        <div className="header-wrapper h-[122px] flex items-center py-0 px-[15px] md:px-[30px] xl:px-[64px]">
+          <div className=" w-1/2 lg:w-2/12">
+            <div className=" flex items-center gap-4 ">
+              <a href="#">
+                <img
+                  src="https://cdn.pixabay.com/photo/2021/06/04/10/28/portrait-6309448_1280.jpg"
+                  className="w-[70px] h-[70px] rounded-full sidebar-img border "
+                  alt="img"
+                />
+              </a>
+              <h1 className=" select-none text-[#c4cfde] text-xl tracking-wider uppercase font-semibold">
+                David
+              </h1>
+            </div>
+          </div>
+          <div className="flex justify-end w-1/2 lg:w-10/12 items-center  ">
+            <div className="lg:hidden block text-[#ff014f]  text-[30px] ">
+              {/* <MobileMenuSidebar /> */}
+              <button className="mobile-menu-toggle" onClick={toggleMenu}>
+                <FaBars />
+              </button>
+            </div>
+            <div className=" primary-menu lg:flex hidden gap-5 nav-text py-9 pr-5">
+              <a
+                href="#home"
+                className={activeSection === "home" ? "active" : ""}
+                onClick={(e) => handleNavClick(e, "home")}
+              >
+                <span className=" nav-text">Home</span>
+              </a>
+              <a
+                href="#experience"
+                className={activeSection === "experience" ? "active" : ""}
+                onClick={(e) => handleNavClick(e, "experience")}
+              >
+                <span className=" nav-text">Experience</span>
+              </a>
+              <a
+                href="#education"
+                className={activeSection === "education" ? "active" : ""}
+                onClick={(e) => handleNavClick(e, "education")}
+              >
+                <span className=" nav-text">Education</span>
+              </a>
+
+              <a
+                href="#resume"
+                className={activeSection === "resume" ? "active" : ""}
+                onClick={(e) => handleNavClick(e, "resume")}
+              >
+                <span className=" nav-text">Resume</span>
+              </a>
+              <a
+                href="#portfolio"
+                className={activeSection === "portfolio" ? "active" : ""}
+                onClick={(e) => handleNavClick(e, "portfolio")}
+              >
+                <span className=" nav-text">Portfolio</span>
+              </a>
+              <a
+                href="#blog"
+                className={activeSection === "blog" ? "active" : ""}
+                onClick={(e) => handleNavClick(e, "blog")}
+              >
+                <span className=" nav-text">Blog</span>
+              </a>
+              <a
+                href="#contact"
+                className={activeSection === "contact" ? "active" : ""}
+                onClick={(e) => handleNavClick(e, "contact")}
+              >
+                <span className=" nav-text">Contact</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <MobileMenuSidebar
+        isMenuOpen={isMenuOpen}
+        toggleMenu={toggleMenu}
+        handleNavClick={handleNavClick}
+      />
+    </>
+  );
+};
+
+export default Navbar;
